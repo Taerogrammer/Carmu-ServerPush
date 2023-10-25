1803edd0b17417b3d68a1e72acadfa6f13b5add2
Taerogrammer <hyung54321@naver.com>
2023-10-25 14:30:07 +0900

    - `lateNotification` : 지각버튼을 클릭했을 때, 나머지 인원에게 푸시 알람을 전송합니다.

--- functions/index.js
+++ functions/index.js
@@ -46,8 +46,7 @@ exports.open_session = onRequest(async (req, res) => {
 // 친구가 추가되었을 때 크루원들에게 서버 푸시를 보내주는 함수
 exports.pushToReceiver = onRequest(async (req, res) => {
     try {
-      // 대상 디바이스의 토큰 배열
-      const token = req.body.data.token; // 이제 tokens 배열로부터 토큰을 가져옵니다.
+      const token = req.body.data.token; // 알림을 보낼 디바이스의 토큰값
       console.log("Token --> ", token)
 
         // 푸시 메시지 설정
@@ -57,7 +56,7 @@ exports.pushToReceiver = onRequest(async (req, res) => {
             title: "카뮤",
             body: "새로운 친구가 추가되었어요!",
             },
-            token: token, // 수정된 토큰 값
+            token: token,
         },
         ];
   
@@ -73,27 +72,26 @@ exports.pushToReceiver = onRequest(async (req, res) => {
     }
   });
 
-  // 늦는다는 알림을 보내주는 함수
-  exports.lateNotification = onRequest(async (req, res) => {
-    try {
-      // 대상 디바이스의 토큰 배열
-      const tokens = req.body.data.tokens; // 이제 tokens 배열로부터 토큰을 가져옵니다.
-      const lateMin = req.body.data.lateMin; // 클라이언트로부터 lateMin 받기
-      console.log("Token --> ", tokens)
-  
-      if (!Array.isArray(tokens)) {
-        return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
-      }
-  
-      // 푸시 메시지 설정
-      const messages = tokens.map((token) => ({
-        notification: {
-          title: "카뮤",
-          body: "누군가가 ${lateMin}분 늦어요!",
-        },
-        token: token,
-      }));
-  
+    // 늦는다는 알림을 보내주는 함수
+    exports.lateNotification = onRequest(async (req, res) => {
+      try {
+        // 대상 디바이스의 토큰 배열
+        const token = req.body.data.token; // 디바이스 토큰값 가져오기
+        const lateMin = req.body.data.lateMin; // 클라이언트로부터 lateMin 받기
+        console.log("Token --> ", token)
+        console.log("late min ", lateMin)
+    
+        // 푸시 메시지 설정
+        const messages = [
+          {
+              notification: {
+              title: "카뮤",
+              body: "누군가가 " + lateMin + "분 늦어요!",
+              },
+              token: token, // 수정된 토큰 값
+          },
+          ];
+    
       // 여러 푸시 메시지 전송
       const responses = await Promise.all(messages.map((message) => messaging.send(message)));
       console.log("Successfully sent messages:", responses);

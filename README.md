50c11a51ae66746dc667068f15184e94bc681177
Taerogrammer <hyung54321@naver.com>
2023-10-31 12:42:10 +0900

    필수 함수 생성 완료

--- README.md
+++ README.md
@@ -17,8 +17,8 @@ Firebase Cloud Functions를 이용하여 구현하였습니다.
 
 ## 필수
 - `journeyStartNotification` : 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
-- `lateNotificationToPassenger` : 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
-- `lateNotificationToDriver` : 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
+- `lateNotificationToPassenger` : 운전자가 지각할 시, 동승자들에게 푸시 알림을 전송합니다.
+- `lateNotificationToDriver` : 동승자가 지각할 시, 운전자에게 푸시 알림을 전송합니다.
 - `giveupNotification` : 여정 도중 동승자가 함께 가기를 포기하였을 때, 운전자에게 푸시 알림을 전송합니다.
 
 <br>
--- functions/index.js
+++ functions/index.js
@@ -12,47 +12,7 @@ const db = admin.database();  // realtime database 접근
 // 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
 exports.journeyStartNotification = onRequest(async (req, res) => {
   try {
-    // 대상 디바이스의 토큰 배열
-    const token = req.body.data.token; // 이제 tokens 배열로부터 토큰을 가져옵니다.
-    console.log("Token --> ", token)
-
-        // 푸시 메시지 설정
-    const messages = [
-      {
-          notification: {
-          title: "카뮤",
-          body: "운전자가 여정을 시작하였어요!",
-          },
-          token: token, // 수정된 토큰 값
-          apns: {
-            payload: {
-              aps: {
-                sound: "default",
-                contentAvailable: true,
-              },
-            },
-          },
-      },
-    ];
-
-    // 여러 푸시 메시지 전송
-    const responses = await Promise.all(messages.map((message) => messaging.send(message)));
-    console.log("Successfully sent messages:", responses);
-
-    res.status(200).json({ success: true, data: responses });
-
-  } catch (error) {
-    console.error("Error Sending message:", error);
-    res.status(500).json({ error: "Failed to send message" });
-  }
-});
-
-// 여정 도중 동승자가 함께 가기를 포기하였을 때, 푸시 알림을 전송합니다.
-exports.giveupNotification = onRequest(async (req, res) => {
-  try {
-    // 누구한테 보낼 지 선정하기!!
-    const tokens = req.body.data.tokens; // 그룹의 동승자들의 토큰값을 받아옵니다.
-    const giveupUserNickname = req.body.data.giveupUserNickname;  // 동승을 포기한 유저의 닉네임을 받아옵니다.
+    const tokens = req.body.data.tokens; // 동승자들의 device token
     console.log("Token --> ", tokens)
 
     if (!Array.isArray(tokens)) {
@@ -63,7 +23,7 @@ exports.giveupNotification = onRequest(async (req, res) => {
     const messages = tokens.map((token) => ({
       notification: {
         title: "카뮤",
-        body: giveupUserNickname + "님이 여정을 포기하였어요.",
+        body: "운전자가 운전을 시작하였어요!",
       },
       token: token,
     }));
@@ -80,7 +40,112 @@ exports.giveupNotification = onRequest(async (req, res) => {
   }
 });
 
-// 동승자가 그룹에 참여하였을 때, 운전자에게 푸시 알림을 전송합니다.
+// 운전자가 지각할 시, 동승자들에게 푸시 알림을 전송합니다.
+exports.lateNotificationToPassenger = onRequest(async (req, res) => {
+  try {
+    const tokens = req.body.data.tokens; // 동승자들의 device token
+    const location = req.body.data.location;  // 늦는 경유지
+    const lateMin = req.body.data.lateMin;  // 늦는 시간
+    console.log("Token --> ", tokens)
+
+    if (!Array.isArray(tokens)) {
+      return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
+    }
+
+    // 푸시 메시지 설정
+    const messages = tokens.map((token) => ({
+      notification: {
+        title: "카뮤",
+        body: location + "에 도착이" + lateMin + "분 지연된다고 해요",
+      },
+      token: token,
+    }));
+
+    // 여러 푸시 메시지 전송
+    const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+    console.log("Successfully sent messages:", responses);
+
+    res.status(200).json({ success: true, data: responses });
+
+  } catch (error) {
+    console.error("Error Sending message:", error);
+    res.status(500).json({ error: "Failed to send message" });
+  }
+});
+
+// 동승자가 지각할 시, 운전자에게 푸시 알림을 전송합니다.
+exports.lateNotificationToDriver = onRequest(async (req, res) => {
+  try {
+
+    const token = req.body.data.token; // 운전자의 device token
+    const lateMin = req.body.data.lateMin; // 클라이언트로부터 lateMin 받기
+    const nickname = req.body.data.nickname;  // 지각한 사람의 닉네임
+
+    console.log("Token --> ", token)
+    console.log("late min ", lateMin)
+    console.log("nickname --> ", nickname)
+
+    // 푸시 메시지 설정
+    const messages = [
+      {
+          notification: {
+          title: "카뮤",
+          body: nickname + "님이 " + lateMin + "분 늦는다고 해요",
+          },
+          token: token, // 수정된 토큰 값
+      },
+      ];
+
+  const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+  console.log("Successfully sent messages:", responses);
+
+  res.status(200).json({ success: true, data: responses });
+
+} catch (error) {
+  console.error("Error Sending message:", error);
+  res.status(500).json({ error: "Failed to send message" });
+}
+});
+
+// 여정 도중 동승자가 함께 가기를 포기하였을 때, 운전자에게 푸시 알림을 전송합니다.
+exports.giveupNotification = onRequest(async (req, res) => {
+  try {
+
+    const token = req.body.data.token; // 운전자의 device token
+    const nickname = req.body.data.nickname;  // 지각한 사람의 닉네임
+    
+    console.log("Token --> ", token)
+    console.log("nickname --> ", nickname)
+
+    // 푸시 메시지 설정
+    const messages = [
+      {
+          notification: {
+          title: "카뮤",
+          body: nickname + "님이 여정을 포기하셨어요",
+          },
+          token: token, // 수정된 토큰 값
+      },
+      ];
+
+  const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+  console.log("Successfully sent messages:", responses);
+
+  res.status(200).json({ success: true, data: responses });
+
+} catch (error) {
+  console.error("Error Sending message:", error);
+  res.status(500).json({ error: "Failed to send message" });
+}
+});
+
+
+
+
+
+  // -----------------------------------------옵셔널-----------------------------------------
+
+  // 동승자가 그룹에 참여하였을 때, 운전자에게 푸시 알림을 전송합니다.
 exports.userJoinedNotification = onRequest(async (req, res) => {
   try {
     // 방장(운전자)의 디바이스 토큰
@@ -111,41 +176,6 @@ exports.userJoinedNotification = onRequest(async (req, res) => {
   res.status(500).json({ error: "Failed to send message" });
   }
   });
-
-
-
-
-  // 옵셔널
-  // 지각버튼을 클릭했을 때, 나머지 인원에게 푸시 알람을 전송합니다.
-exports.lateNotification = onRequest(async (req, res) => {
-  try {
-    // 대상 디바이스의 토큰 배열
-    const token = req.body.data.token; // 디바이스 토큰값 가져오기
-    const lateMin = req.body.data.lateMin; // 클라이언트로부터 lateMin 받기
-    console.log("Token --> ", token)
-    console.log("late min ", lateMin)
-
-    // 푸시 메시지 설정
-    const messages = [
-      {
-          notification: {
-          title: "카뮤",
-          body: "누군가가 " + lateMin + "분 늦어요!",
-          },
-          token: token, // 수정된 토큰 값
-      },
-      ];
-
-  const responses = await Promise.all(messages.map((message) => messaging.send(message)));
-  console.log("Successfully sent messages:", responses);
-
-  res.status(200).json({ success: true, data: responses });
-
-} catch (error) {
-  console.error("Error Sending message:", error);
-  res.status(500).json({ error: "Failed to send message" });
-}
-});
 
 // 누군가가 그룹을 나갔을 때, 해당 그룹 인원들에게 푸시 알림을 전송합니다.
 exports.leftGroupNotification = onRequest(async (req, res) => {

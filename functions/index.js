17e7aae8aeaeafcd708c65d9e5e079a62df09e1e
Taerogrammer <hyung54321@naver.com>
2023-10-31 12:22:40 +0900

    파일 및 필요한 메서드 정리

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,77 @@ const firestore = admin.firestore();
 const messaging = admin.messaging();
 const db = admin.database();  // realtime database 접근
 
+// 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
+exports.journeyStartNotification = onRequest(async (req, res) => {
+  try {
+    // 대상 디바이스의 토큰 배열
+    const token = req.body.data.token; // 이제 tokens 배열로부터 토큰을 가져옵니다.
+    console.log("Token --> ", token)
+
+        // 푸시 메시지 설정
+    const messages = [
+      {
+          notification: {
+          title: "카뮤",
+          body: "운전자가 여정을 시작하였어요!",
+          },
+          token: token, // 수정된 토큰 값
+          apns: {
+            payload: {
+              aps: {
+                sound: "default",
+                contentAvailable: true,
+              },
+            },
+          },
+      },
+    ];
+
+    // 여러 푸시 메시지 전송
+    const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+    console.log("Successfully sent messages:", responses);
+
+    res.status(200).json({ success: true, data: responses });
+
+  } catch (error) {
+    console.error("Error Sending message:", error);
+    res.status(500).json({ error: "Failed to send message" });
+  }
+});
+
+// 여정 도중 동승자가 함께 가기를 포기하였을 때, 푸시 알림을 전송합니다.
+exports.giveupNotification = onRequest(async (req, res) => {
+  try {
+    // 누구한테 보낼 지 선정하기!!
+    const tokens = req.body.data.tokens; // 그룹의 동승자들의 토큰값을 받아옵니다.
+    const giveupUserNickname = req.body.data.giveupUserNickname;  // 동승을 포기한 유저의 닉네임을 받아옵니다.
+    console.log("Token --> ", tokens)
+
+    if (!Array.isArray(tokens)) {
+      return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
+    }
+
+    // 푸시 메시지 설정
+    const messages = tokens.map((token) => ({
+      notification: {
+        title: "카뮤",
+        body: giveupUserNickname + "님이 여정을 포기하였어요.",
+      },
+      token: token,
+    }));
+
+    // 여러 푸시 메시지 전송
+    const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+    console.log("Successfully sent messages:", responses);
+
+    res.status(200).json({ success: true, data: responses });
+
+  } catch (error) {
+    console.error("Error Sending message:", error);
+    res.status(500).json({ error: "Failed to send message" });
+  }
+});
+
 // 동승자가 그룹에 참여하였을 때, 운전자에게 푸시 알림을 전송합니다.
 exports.userJoinedNotification = onRequest(async (req, res) => {
   try {
@@ -41,6 +112,10 @@ exports.userJoinedNotification = onRequest(async (req, res) => {
   }
   });
 
+
+
+
+  // 옵셔널
   // 지각버튼을 클릭했을 때, 나머지 인원에게 푸시 알람을 전송합니다.
 exports.lateNotification = onRequest(async (req, res) => {
   try {
@@ -71,38 +146,6 @@ exports.lateNotification = onRequest(async (req, res) => {
   res.status(500).json({ error: "Failed to send message" });
 }
 });
-
-// 운전을 시작하였을 때, 동승자들에게 푸시 알림을 전송합니다.
-exports.journeyStartNotification = onRequest(async (req, res) => {
-    try {
-      // 대상 디바이스의 토큰 배열
-      const tokens = req.body.data.tokens; // 이제 tokens 배열로부터 토큰을 가져옵니다.
-      console.log("Token --> ", tokens)
-  
-      if (!Array.isArray(tokens)) {
-        return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
-      }
-  
-      // 푸시 메시지 설정
-      const messages = tokens.map((token) => ({
-        notification: {
-          title: "카뮤",
-          body: "운전자가 여정을 시작하였어요!",
-        },
-        token: token,
-      }));
-  
-      // 여러 푸시 메시지 전송
-      const responses = await Promise.all(messages.map((message) => messaging.send(message)));
-      console.log("Successfully sent messages:", responses);
-  
-      res.status(200).json({ success: true, data: responses });
-  
-    } catch (error) {
-      console.error("Error Sending message:", error);
-      res.status(500).json({ error: "Failed to send message" });
-    }
-  });
 
 // 누군가가 그룹을 나갔을 때, 해당 그룹 인원들에게 푸시 알림을 전송합니다.
 exports.leftGroupNotification = onRequest(async (req, res) => {
@@ -168,35 +211,37 @@ exports.groupInfoChangedNotification = onRequest(async (req, res) => {
   }
 });
 
-// 여정 도중 동승자가 함께 가기를 포기하였을 때, 푸시 알림을 전송합니다.
-exports.giveupNotification = onRequest(async (req, res) => {
-  try {
-    // 누구한테 보낼 지 선정하기!!
-    const tokens = req.body.data.tokens; // 그룹의 동승자들의 토큰값을 받아옵니다.
-    const giveupUserNickname = req.body.data.giveupUserNickname;  // 동승을 포기한 유저의 닉네임을 받아옵니다.
-    console.log("Token --> ", tokens)
 
-    if (!Array.isArray(tokens)) {
-      return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
-    }
 
-    // 푸시 메시지 설정
-    const messages = tokens.map((token) => ({
-      notification: {
-        title: "카뮤",
-        body: giveupUserNickname + "님이 여정을 포기하였어요.",
-      },
-      token: token,
-    }));
+// 배열 처리할 때
+// // 누군가가 그룹을 나갔을 때, 해당 그룹 인원들에게 푸시 알림을 전송합니다.
+// exports.leftGroupNotification = onRequest(async (req, res) => {
+//   try {
+//     const tokens = req.body.data.tokens; // 이제 tokens 배열로부터 토큰을 가져옵니다.
+//     const leftUserNickname = req.body.data.nickname;  // 나간 유저의 닉네임을 불러옵니다.
+//     console.log("Token --> ", tokens)
 
-    // 여러 푸시 메시지 전송
-    const responses = await Promise.all(messages.map((message) => messaging.send(message)));
-    console.log("Successfully sent messages:", responses);
+//     if (!Array.isArray(tokens)) {
+//       return res.status(400).json({ error: "Invalid 'tokens' parameter. It should be an array." });
+//     }
 
-    res.status(200).json({ success: true, data: responses });
+//     // 푸시 메시지 설정
+//     const messages = tokens.map((token) => ({
+//       notification: {
+//         title: "카뮤",
+//         body: leftUserNickname + "님이 그룹을 떠났어요.",
+//       },
+//       token: token,
+//     }));
 
-  } catch (error) {
-    console.error("Error Sending message:", error);
-    res.status(500).json({ error: "Failed to send message" });
-  }
-});
+//     // 여러 푸시 메시지 전송
+//     const responses = await Promise.all(messages.map((message) => messaging.send(message)));
+//     console.log("Successfully sent messages:", responses);
+
+//     res.status(200).json({ success: true, data: responses });
+
+//   } catch (error) {
+//     console.error("Error Sending message:", error);
+//     res.status(500).json({ error: "Failed to send message" });
+//   }
+// });
